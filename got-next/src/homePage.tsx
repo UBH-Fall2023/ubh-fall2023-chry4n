8909622d724dbbd6048077648b7597c1ae558171
edowner12 <137216271+edowner12@users.noreply.github.com>
2023-11-04 14:47:49 -0400

    blue box
    Makes the whole screen blue

---
+++ got-next/src/homePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import './App.css'; 
+export default function App() {
+    return(
+        <main>
+            <div className = 'bg-blue-300 h-full w-full' 
+            ></div>
+        </main>
+    )
+}
