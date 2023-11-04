c485157e5ab2076472d285242d85e3f2aade0b43
edowner12 <emdowner@buffalo.edu>
2023-11-04 17:05:08 -0400

    blue box

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+
 export default function Home() {
   return (
-    <div className= ''>
+    <div className= 'bg-blue-300 h-1/2 w-1/2'>
       hello
     </div>
   );
