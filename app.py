26c610ad2e78659dc9c2e34a5f971adfd7b659d9
Michael <mjliao@buffalo.edu>
2023-11-04 19:06:06 -0400

    cam

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/next')
+def next_page():
+    return render_template('next.html')
+
+
 if __name__ == "__main__":
     app.run(debug = True)
